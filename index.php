605787510409e373af768c11f2398fb46a2c7f73
dimastriat <113988946+dimastriat@users.noreply.github.com>
2023-05-30 18:57:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,9 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>mywebsite</title>
+    <title>Praktikum STI Kelas I</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
   </head>
   <body>
     <!-- Navbar -->
@@ -53,8 +54,8 @@
 <div class="container-md">
     <div class="jumbotron jumbotron-fluid" >
         <div class="container " >
-          <h1 class="display-4">Selamat Datang di Laboratorium Komputasi dan Sistem Agroindustri</h1>
-          <p class="lead">Laboratorium terbaik untuk masa depan cerah anda.</p>
+          <h1 class="display-4">Terima Kasih Banyak untuk Seluruh Teman-Teman Praktikan STI Kelas I</h1>
+          <p class="lead">Semoga ilmunya bermanfaat dan nilainya memuaskan :)</p>
         </div>
       </div>
       <!-- Slider-->
@@ -63,20 +64,24 @@
           <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
           <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
           <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 3"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
         </div>
         <div class="carousel-inner">
           <div class="carousel-item active">
-            <img src="img/01.jpg" class="d-block w-100" alt="...">
+            <img src="img/09.jpg" class="d-block w-100" alt="...">
           </div>
           <div class="carousel-item">
-            <img src="img/02.jpg" class="d-block w-100" alt="...">
+            <img src="img/07.jpg" class="d-block w-100" alt="...">
           </div>
           <div class="carousel-item">
-            <img src="img/03.jpg" class="d-block w-100" alt="...">
+            <img src="img/08.jpg" class="d-block w-100" alt="...">
           </div>
           <div class="carousel-item">
-            <img src="img/04.jpg" class="d-block w-100" alt="...">
+            <img src="img/06.jpg" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="img/10.jpg" class="d-block w-100" alt="...">
           </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
@@ -93,39 +98,49 @@
 <!--Container 2-->
 <div class="border-bottom pb-3 mb-5 container text-center shadow">
     <!--Card-->
-    <h1><b>PRAKTIKUM LAB KSA</b></h1>
+    <h1><b>WEJANGAN ASPRAK</b></h1>
     <div class="card-group">
   <div class="card">
     <img src="img/01.jpg" class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title">Gambar Teknik</h5>
-      <p class="card-text">Mata kuliah ini bertujuan untuk mengajarkan mahasiswa tentang prinsip-prinsip dasar dalam pembuatan dan interpretasi gambar teknik.</p>
-      <p class="card-text"><small class="text-muted">TPI61009</small></p>
+      <h5 class="card-title">Mas Zaa</h5>
+      <p class="card-text">"Terima kasih banyak temen2 atas partisipasinya selama ini di praktikum STI. Semoga apa yg udah sama2 kita pelajari bisa bermanfaat untuk ke depannya. Semangat selalu dalam menjalani perkuliahan selanjutnya dan yang terpenting jangan lupa bahagia :)".</p>
+      <a href="https://instagram.com/zaa__r" class='btn btn-light btn-sm'><i class="bi bi-instagram"></i></a>
+      <a href="https://wa.me/+6281584824947" class='btn btn-light btn-sm'><i class="bi bi-whatsapp"></i></a>
     </div>
   </div>
   <div class="card">
     <img src="img/02.jpg" class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title">Pemrograman Komputer</h5>
-      <p class="card-text">Mata kuliah ini bertujuan untuk mengajarkan mahasiswa tentang dasar-dasar pemrograman dan rekayasa sistem agroindustri.</p>
-      <p class="card-text"><small class="text-muted">TPI62015</small></p>
-    </div>
+      <h5 class="card-title">Mas Yos</h5>
+      <p class="card-text">"select* from praksti_kelasF; Terima kasih teman-teman kelas F kalian hebat!! semangat buat kedepannya dan sampai jumpa di praktikum lainnya.  ~Look at the stars
+Look how they shine for you. (Aldi Taher-Yellow)~"</p>
+      <a href="https://instagram.com/yosafatchrz" class='btn btn-light btn-sm'><i class="bi bi-instagram"></i></a>
+      <a href="https://wa.me/+6285604451835" class='btn btn-light btn-sm'><i class="bi bi-whatsapp"></i></a>    </div>
   </div>
   <div class="card">
     <img src="img/03.jpg" class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title">Statistika Industri</h5>
-      <p class="card-text">Mata kuliah ini bertujuan untuk mengajarkan mahasiswa tentang penerapan statistika dalam agroindustri.</p>
-      <p class="card-text"><small class="text-muted">TPI61024</small></p>
-    </div>
+      <h5 class="card-title">Mbak Sil</h5>
+      <p class="card-text">"Semangat teman-teman kelas F, respect kalian kece abiez! mohon maaf kalau ada kekurangan selama kami mengajar"</p>
+      <a href="https://instagram.com/silviaarsitaa" class='btn btn-light btn-sm'><i class="bi bi-instagram"></i></a>
+      <a href="https://wa.me/+6281322683454" class='btn btn-light btn-sm'><i class="bi bi-whatsapp"></i></a>    </div>
   </div>
   <div class="card">
     <img src="img/04.jpg" class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title">Sistem Teknologi Informasi</h5>
-      <p class="card-text">Mata kuliah ini bertujuan untuk memberikan pemahaman tentang konsep dasar, prinsip, dan aplikasi teknologi informasi dalam agroindustri.</p>
-      <p class="card-text"><small class="text-muted">TPI62042</small></p>
-    </div>
+      <h5 class="card-title">Mas Ri</h5>
+      <p class="card-text">"Terima kasih teman2 praktikan yang udah berpartisipasi aktif selama satu semester ini. Semoga ga kapok sama STI dan bisa dapet nilai bagus di UAP. STI praktikum yang mudah bukan? BUKAN!"</p>
+      <a href="https://instagram.com/mfarras.a" class='btn btn-light btn-sm'><i class="bi bi-instagram"></i></a>
+      <a href="https://wa.me/+6285887623768" class='btn btn-light btn-sm'><i class="bi bi-whatsapp"></i></a>    </div>
+  </div>
+  <div class="card">
+    <img src="img/05.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Mas Bon</h5>
+      <p class="card-text">"Tidak berasa yaa temen2 kita sudah berada di kelas terakhir prak sti, terimakasih buat temen2 atas partisipasi dan niatnya buat belajar praktikum ini (walau sedikit susah). Semoga yang kita pelajarin bareng2 dapat bermanfaat dan bisa buat bekal temen2 di masa depan. Aamiin,"</p>
+      <a href="https://instagram.com/janbagas" class='btn btn-light btn-sm'><i class="bi bi-instagram"></i></a>
+      <a href="https://wa.me/+6285791943291" class='btn btn-light btn-sm'><i class="bi bi-whatsapp"></i></a>    </div>
   </div>
 </div>
 </div>
@@ -133,22 +148,9 @@
 <!--Footer-->
 <footer class="py-3 my-4">
   <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-<ul class="nav justify-content-center">
-  <li class="nav-item">
-    <a class="nav-link active" href="instagram.com">Active</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link disabled">Disabled</a>
-  </li>
+    <a href="https://forms.gle/L3C4yDYX6c1yPBdcA" class="btn btn-danger">KESAN PESAN & EVALUASI</a>
 </ul>
-
-
+ 
 <script src="js/bootstrap.bundle.min.js"></script>
 
 </body>
